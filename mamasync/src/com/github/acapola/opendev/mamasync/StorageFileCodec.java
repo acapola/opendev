9b61f7a222d5b8fd4c80495bb9247c81ecdd51c4
acapola <matic@nimp.co.uk>
2014-06-14 18:49:37 -0400

    .

---
+++ mamasync/src/com/github/acapola/opendev/mamasync/StorageFileCodec.java
@@ -0,0 +1,7 @@
+package com.github.acapola.opendev.mamasync;
+
+/**
+ * Created by seb on 6/14/14.
+ */
+public abstract class StorageFileCodec {
+}
---
+++ mamasync/src/com/github/acapola/opendev/mamasync/SyncTree.java
@@ -0,0 +1,13 @@
+package com.github.acapola.opendev.mamasync;
+
+/**
+ * Created by seb on 6/14/14.
+ */
+public class SyncTree {
+    final StorageDirectory storage;
+    final WorkDirectory work;
+    public SyncTree(){
+        storage=null;//TODO
+        work=null;//TODO
+    }
+}
