5173bcf31df72464b285133f928d7e5adce603f8
acapola <matic@nimp.co.uk>
2015-03-01 16:04:23 +0100

    lfsr
    more mersenne numbers

--- lfsr/tgpp/factor.tcl
+++ lfsr/tgpp/factor.tcl
@@ -1,9 +1,66 @@
 
 
 proc factor { num } {
-    set res [exec yafu factor($num)]
-    set res [string range $res [string first "\nP" $res] end]
-    set res [string range $res 0 [string first "\nans" $res]]
+    #set res [exec yafu factor($num)]
+	if {[file exists factor.log]} {
+		#this log files can grow very big, clean it.
+		file delete factor.log
+	}
+	set status [catch {exec yafu factor($num)} res]
+	if {$status == 0} {
+		# The command succeeded, and wrote nothing to stderr.
+		# $result contains what it wrote to stdout, unless you
+		# redirected it
+	} elseif {[string equal $::errorCode NONE]} {
+		# The command exited with a normal status, but wrote something
+		# to stderr, which is included in $result.
+	} else {
+		switch -exact -- [lindex $::errorCode 0] {
+			CHILDKILLED {
+				puts CHILDKILLED
+				foreach {- pid sigName msg} $::errorCode break
+
+				# A child process, whose process ID was $pid,
+				# died on a signal named $sigName.  A human-
+				# readable message appears in $msg.
+
+			}
+			CHILDSTATUS {
+				puts CHILDSTATUS
+				foreach {- pid code} $::errorCode break
+
+				# A child process, whose process ID was $pid,
+				# exited with a non-zero exit status, $code.
+
+			}
+
+			CHILDSUSP {
+				puts CHILDSUSP
+				foreach {- pid sigName msg} $::errorCode break
+
+				# A child process, whose process ID was $pid,
+				# has been suspended because of a signal named
+				# $sigName.  A human-readable description of the
+				# signal appears in $msg.
+
+			}
+
+			POSIX {
+				puts POSIX
+				foreach {- errName msg} $::errorCode break
+
+				# One of the kernel calls to launch the command
+				# failed.  The error code is in $errName, and a
+				# human-readable message is in $msg.
+
+			}
+
+		}
+	}
+	set pPos [string first "\nP" $res]
+    set res [string range $res $pPos end]
+	set ansPos [string first "\nans" $res]
+    set res [string range $res 0 $ansPos]
     #puts $res
     set lines [split $res "\n"]
     set factors [dict create]
